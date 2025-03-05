24b70b3ebdfb998663a826bb2196563b83038166
reynalfawwaz <148095338+reynalfawwaz@users.noreply.github.com>
2025-03-05 19:43:13 +0700

    Add files via upload

---
+++ WADweb/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Personal Web - PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1><?php echo "A Web Page"; ?></h1>
+    <div class="container">
+        <div class="profile-pic">
+            <img src="assets/foto.jpg">
+        </div>
+        <h2><?php echo "Muhammad Reynal Fawwaz Abyasa / 1202220272 / Fakultas FRI / S1 Sistem Informasi"; ?></h2>
+        <div class="social-media">
+            <a href="https://github.com" target="_blank">Github</a>
+            <a href="https://instagram.com" target="_blank">Instagram</a>
+        </div>
+    </div>
+</body>
+</html>
